3c5a44118fadd8236709bb38116e773a3e2514c5
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2020-04-24 01:51:50 -0400

    update json file

--- living/src/App.js
+++ living/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Dashboard from "./Components/Dashboard.js";
-//import Login from "./pages/login";
+import Login from "./pages/login";
 import Home from "./Components/Home";
 
 class App extends Component {
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="app">
         <BrowserRouter>
           <Switch>
-            <Route exact path={"/"} component={Home} />
+            <Route exact path={"/"} component={Login} />
             <Route exact path={"/dashboard"} component={Dashboard} />
           </Switch>
         </BrowserRouter>
