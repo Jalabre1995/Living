02f7cf0ae3572c62362599fced19a4d894001100
Justin Durocher <justduro@gmail.com>
2020-04-24 01:48:14 -0400

    merge

--- living/src/Components/Home.js
+++ living/src/Components/Home.js
@@ -5,7 +5,10 @@ import { Button } from "react-mdl";
 import { bounceInDown, fadeIn } from "react-animations";
 import Radium, {StyleRoot} from 'radium';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import './css/Home.css';
+=======
+>>>>>>> bc8171c4dbd6014cee0e47d7fbb04a7e432806ff
 =======
 >>>>>>> bc8171c4dbd6014cee0e47d7fbb04a7e432806ff
 
@@ -34,6 +37,7 @@ export default class Home extends Component {
 
             <StyleRoot>
 <<<<<<< HEAD
+<<<<<<< HEAD
 
             <img src={House} alt="home" height="200px" width="200px" style={styles.bounceInDown}/>
 
@@ -55,6 +59,17 @@ export default class Home extends Component {
 >>>>>>> bc8171c4dbd6014cee0e47d7fbb04a7e432806ff
             <p className="fade" >Welcome to the Application that will help you find a better place to live!</p>
 
+=======
+
+            <img src={House} alt="home" height="200px" width="200px" style={styles.bounceInDown}/>
+
+            </StyleRoot>
+            
+            <h1 className="header" stye={styles.fadeIn}>Living.js</h1>
+
+            <p className="fade" >Welcome to the Application that will help you find a better place to live!</p>
+
+>>>>>>> bc8171c4dbd6014cee0e47d7fbb04a7e432806ff
             <Button raised colored className="fade hover">Begin Search</Button>
 
             </div>
