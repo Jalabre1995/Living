d367b267359d5c6adb81485185a2d9764e342594
Justin Durocher <justduro@gmail.com>
2020-04-23 20:48:52 -0400

    home page

--- living/src/Components/Home.js
+++ living/src/Components/Home.js
@@ -1,24 +1,18 @@
 import React, {Component} from 'react';
-import Registration from './auth/Registration';
-import Login from './auth/Login';
 import './css/Home.css';
 
 export default class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
-    }
-    handleSuccessfulAuth(data) {
-        this.props.handleLogin(data);
-        this.props.history.push('/dashboard');
-    }
+ 
+
     render() {
         return(
-            <div>
-            <h1>Home</h1>
-            <h1>Status: {this.props.loggedInStatus}</h1>
-            <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
-            <Login handleSuccessfulAuth ={this.handleSuccessfulAuth} />
+            <div className="home">
+            
+            <h1 className="header">Living</h1>
+
+            <p className="quote">Welcome to the Application that will help you find a better place to live!</p>
+
+
             </div>
         )
     }
