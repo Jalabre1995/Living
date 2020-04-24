259849d130853f74f9ed1449ede7b304cf251713
Justin Durocher <justduro@gmail.com>
2020-04-24 01:53:37 -0400

    changes

--- living/src/Components/Home.js
+++ living/src/Components/Home.js
@@ -4,13 +4,8 @@ import House from './images/home.png'
 import { Button } from "react-mdl";
 import { bounceInDown, fadeIn } from "react-animations";
 import Radium, {StyleRoot} from 'radium';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import './css/Home.css';
-=======
->>>>>>> bc8171c4dbd6014cee0e47d7fbb04a7e432806ff
-=======
->>>>>>> bc8171c4dbd6014cee0e47d7fbb04a7e432806ff
+
 
 const styles = {
 
@@ -36,8 +31,6 @@ export default class Home extends Component {
             <div className="home">
 
             <StyleRoot>
-<<<<<<< HEAD
-<<<<<<< HEAD
 
             <img src={House} alt="home" height="200px" width="200px" style={styles.bounceInDown}/>
 
@@ -48,29 +41,6 @@ export default class Home extends Component {
             <p className="fade" >Welcome to the Application that will help you find the best place to live!</p>
 
             <Button raised colored className="fade">Begin Search</Button>
-=======
-
-            <img src={House} alt="home" height="200px" width="200px" style={styles.bounceInDown}/>
-
-            </StyleRoot>
-            
-            <h1 className="header" stye={styles.fadeIn}>Living.js</h1>
-
->>>>>>> bc8171c4dbd6014cee0e47d7fbb04a7e432806ff
-            <p className="fade" >Welcome to the Application that will help you find a better place to live!</p>
-
-=======
-
-            <img src={House} alt="home" height="200px" width="200px" style={styles.bounceInDown}/>
-
-            </StyleRoot>
-            
-            <h1 className="header" stye={styles.fadeIn}>Living.js</h1>
-
-            <p className="fade" >Welcome to the Application that will help you find a better place to live!</p>
-
->>>>>>> bc8171c4dbd6014cee0e47d7fbb04a7e432806ff
-            <Button raised colored className="fade hover">Begin Search</Button>
 
             </div>
         )
