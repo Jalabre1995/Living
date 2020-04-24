33547f19ef1f10209035c6b008c8c4734c4a1e66
Justin Durocher <justduro@gmail.com>
2020-04-24 02:11:08 -0400

    merge

--- living/src/App.js
+++ living/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Dashboard from "./Components/Dashboard.js";
-<<<<<<< HEAD
 //import Login from "./pages/login";
-=======
 import Login from "./pages/login";
->>>>>>> 4cc9efebad417dac264257e515424ae5cb358fc1
+
+
 import Home from "./Components/Home";
 
 class App extends Component {
@@ -14,11 +13,13 @@ class App extends Component {
       <div className="app">
         <BrowserRouter>
           <Switch>
-<<<<<<< HEAD
+
             <Route exact path={"/"} component={Home} />
-=======
+
             <Route exact path={"/"} component={Login} />
->>>>>>> 4cc9efebad417dac264257e515424ae5cb358fc1
+
+            <Route exact path={"/"} component={Login} />
+
             <Route exact path={"/dashboard"} component={Dashboard} />
           </Switch>
         </BrowserRouter>
