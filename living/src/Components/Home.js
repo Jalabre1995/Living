4ae6d6a630a9e136ef8269fe3ba9af97584cae85
Justin Durocher <justduro@gmail.com>
2020-04-23 21:19:39 -0400

    homepage done

--- living/src/Components/Home.js
+++ living/src/Components/Home.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import './css/Home.css';
+import House from './images/home.png'
+import { Button } from "react-mdl";
 
 export default class Home extends Component {
  
@@ -7,10 +9,14 @@ export default class Home extends Component {
     render() {
         return(
             <div className="home">
+
+            <img src={House} alt="home" height="200px" width="200px"/>
             
             <h1 className="header">Living</h1>
 
             <p className="quote">Welcome to the Application that will help you find a better place to live!</p>
+
+            <Button raised colored>Begin Search</Button>
 
 
             </div>
