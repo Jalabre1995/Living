93550f2675e81650fe3ca85bd626ea8d3069e470
Justin Durocher <justduro@gmail.com>
2020-04-28 18:34:57 -0400

    dash

--- living/src/Components/Dashboard.js
+++ living/src/Components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Navbar, Form, FormControl, Button, Nav} from 'react-bootstrap';
+import {Navbar, Form, FormControl, Button, Nav, Col, Row, Container, Jumbotron, Card, ListGroup, Badge} from 'react-bootstrap';
 import House from './images/home.png';
 import './css/Dashboard.css';
 
@@ -42,6 +42,44 @@ export default class Dashboard extends Component{
 
                     </Form>
                 </Navbar>
+
+                <Container fluid>
+
+                    <Row>
+
+                        <Col sm="2">
+
+                            <Card style={{ width: '18rem' }}>
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item className="list1">Past Searches</ListGroup.Item>
+                                    <ListGroup.Item>Miami</ListGroup.Item>
+                                    <ListGroup.Item>Houston</ListGroup.Item>
+                                    <ListGroup.Item>Las Vegas</ListGroup.Item>
+                                    <ListGroup.Item>Seattle</ListGroup.Item>
+                                    <ListGroup.Item>Washington</ListGroup.Item>
+                                </ListGroup>
+                            </Card>
+
+                        </Col>
+
+                        <Col sm="10">
+
+                            <Jumbotron>
+                                <h1>Welcome!</h1>
+                                <p>
+                                    This is a simple hero unit, a simple jumbotron-style component for calling
+                                    extra attention to featured content or information.
+                                </p>
+                                <p>
+                                    <Button variant="primary">Learn more</Button>
+                                </p>
+                            </Jumbotron>
+
+                        </Col>
+
+                    </Row>
+
+                </Container>
 
             </div>
 
