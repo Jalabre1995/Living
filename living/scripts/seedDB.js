47cc765f39b6ffe3d7ce69f1fc85b8eb9921eba6
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-04-29 12:24:21 -0400

    updated SeedDB.js

--- living/scripts/seedDB.js
+++ living/scripts/seedDB.js
@@ -4,6 +4,38 @@ const db = require('../models/city.js');
 
 mongoose.connect(
     process.env.MONGODB_URI ||
-    'mongodb://localhost/citylist'
+    'mongodb://localhost/citylist' 
+    
 );
+
+const citySeed = [
+    {
+        costofLiving: '',
+        housing: '',
+        startUps: '',
+        ventrueCapital: '',
+        travelConnectivity: '',
+        commute: '',
+        businessFreedom: '',
+        safety: '',
+        healthCare:'',
+        education: '',
+        enviormentalQuality: '',
+        economy: '',
+        iternetAccess: '',
+
+    }
+];
+
+db.citySeed
+.remove({})
+.then(() => db.City.collection.insertMany(citySeed))
+.then(data => {
+    console.log(data.result.n + ' records inserted!');
+    process.exit(0);
+})
+.catch(err => {
+    console.error(err);
+    process.exit(1)
+});
 
