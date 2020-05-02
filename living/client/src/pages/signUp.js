e7f88756b2890abc8bbd91db1b626c167d4684d1
ramalimanov <imanov.r@yahoo.com>
2020-05-02 13:10:45 -0400

    Up

--- living/client/src/pages/signUp.js
+++ living/client/src/pages/signUp.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-<<<<<<< HEAD
 
 class SignUp extends Component {
     constructor() {
@@ -54,85 +53,6 @@ class SignUp extends Component {
           </div>
         );
     }
-=======
-class SignUp extends Component {
-  state = {
-    email: "",
-    password: "",
-    loginErrors: "",
-  };
-
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    console.log(name, value);
-
-    this.setState({
-      [name]: value,
-    });
-  };
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const { email, password } = this.state;
-    console.log(email, password);
-
-    axios({
-      url: "/authentication/signup",
-      method: "POST",
-
-      data: {
-        email,
-        password,
-      },
-    })
-      .then((response) => {
-        console.log("data:", response, this.state);
-        this.props.history.push("/Home");
-      })
-      .catch((error) => {
-        console.log("Error", error.response);
-      });
-  };
-
-  render() {
-    return (
-      <div className="FormCenter">
-        <form onSubmit={this.handleSubmit} className="FormFields">
-          <div className="FormField">
-            <label className="FormField__Label" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="FormField__Input"
-              placeholder="Enter your password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="FormField">
-            <label className="FormField__Label" htmlFor="email">
-              E-Mail Address
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="FormField__Input"
-              placeholder="Enter your email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="FormField">
-            <button className="FormField__Button mr-20">Sign Up</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
->>>>>>> d72b7b32b8b44cafef90376c2924de7340ceb41f
 }
 
 export default SignUp;
